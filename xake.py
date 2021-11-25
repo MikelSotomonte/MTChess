fac8efb3b35ed8a5d71a3c8e419a2f0a224ba2e9
JulenElia <88969536+JulenElia@users.noreply.github.com>
2021-11-25 20:08:00 +0100

    Update xake.py

--- xake.py
+++ xake.py
@@ -26,12 +26,12 @@ LUT = {
     '-': '  ',
 }
 board = [['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
-         ['bP', 'bP', 'bP', '-', '-', 'bP', 'bP', '-'],
+         ['bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP', 'bP'],
          ['-',  '-',  '-',  '-',  '-',  '-',  '-',  '-' ],
          ['-',  '-',   '-',  '-',  '-',  '-',  '-',  '-' ],
          ['-',  '-',  '-',  '-',   '-',  '-',  '-',  '-' ],
          ['-',  '-',  '-',  '-',  '-',  '-',  '-',  '-' ],
-         ['wP', 'wP', 'wP', '-', '-', 'wP', 'wP', '-'],
+         ['wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP', 'wP'],
          ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']]
 
 def keyInput(key):
@@ -68,34 +68,35 @@ def opositeTurn(turn):
     else: turn = 'w'
     return turn
 
-def movementsRaycast(a,b):
+def movementsRaycast(x,y,rango):
     global selectedPos
     global board
     global turn
     global positions
     
-    for i in range(1, 8, 1):
-        if selectedPos[0]+i*a <= 7 and selectedPos[0]+i*a >= 0 and selectedPos[1]+i*b <= 7 and selectedPos[1]+i*b >= 0:
-            if board[selectedPos[0]+i*a][selectedPos[1]+i*b][0] == '-': #spaces
-                positions.append([selectedPos[0]+i*a, selectedPos[1]+i*b])
-            elif board[selectedPos[0]+i*a][selectedPos[1]+i*b][0] == turn: #same color pices
+    for i in range(1, rango, 1):
+        if selectedPos[0]+i*x <= 7 and selectedPos[0]+i*x >= 0 and selectedPos[1]+i*y <= 7 and selectedPos[1]+i*y >= 0:
+            if board[selectedPos[0]+i*x][selectedPos[1]+i*y][0] == '-': #spaces
+                positions.append([selectedPos[0]+i*x, selectedPos[1]+i*y])
+            elif board[selectedPos[0]+i*x][selectedPos[1]+i*y][0] == turn: #same color pices
                 break
-            elif board[selectedPos[0]+i*a][selectedPos[1]+i*b][0] == opositeTurn(turn): #other color pices to stop moving after that
-                positions.append([selectedPos[0]+i*a, selectedPos[1]+i*b])
+            elif board[selectedPos[0]+i*x][selectedPos[1]+i*y][0] == opositeTurn(turn): #other color pices to stop moving after that
+                positions.append([selectedPos[0]+i*x, selectedPos[1]+i*y])
                 break
+
 def promotePawn():
     global LUT
     global turn
+  
     global cursorPos
-    print('Select pice to promote to:\n    ╔═══╦═══╦═══╦═══╗\n    ║ ', end='')
-    for i in range(): #only compute from first pice to last for cursor rendering
-        if cursorPos[1] == 0: print(LUT[turn + 'Q'])
-        elif cursorPos[1] == 1: print(LUT[turn + 'R'])
-        elif cursorPos[1] == 2: print(LUT[turn + 'B'])
-        elif cursorPos[1] == 3: print(LUT[turn + 'N'])
+    # print('Select pice to promote to:\n    ╔═══╦═══╦═══╦═══╗\n    ║ ', end='')
+    # for i
+    # sorPos[1] == 3: print(LUT[turn + 'N'])
     #print('Select pice to promote to:\n    ╔═══╦═══╦═══╦═══╗\n    ║ {}║ {}║ {}║ {}║\n    ╚═══╩═══╩═══╩═══╝'.format(LUT[turn + 'Q'], LUT[turn + 'R'], LUT[turn + 'B'], LUT[turn + 'N']))
-    print('║\n    ╚═══╩═══╩═══╩═══╝')
-promotePawn()
+  
+
+    # print('║\n    ╚═══╩═══╩═══╩═══╝')
+# promotePawn()
 def selectOrMove():
     global selectedPos
     global turn
@@ -104,54 +105,71 @@ def selectOrMove():
     if board[cursorPos[0]][cursorPos[1]][0] == turn:
         selectedPos = cursorPos.copy()
         positions = []
-        #===ROOK===#
-        if board[cursorPos[0]][cursorPos[1]][1] == 'R':
-            movementsRaycast(1,0)
-            movementsRaycast(0,1)
-            movementsRaycast(-1,0)
-            movementsRaycast(0,-1)
-        #===BISHOP===#
+        #===Rook===#
+        if board[cursorPos[0]][cursorPos[1]][1] == 'R': #selected piece 
+            movementsRaycast(1,0,8) #right
+            movementsRaycast(0,1,8) #up
+            movementsRaycast(-1,0,8) #left
+            movementsRaycast(0,-1,8) #down
+        #===Bishop===#
         if board[cursorPos[0]][cursorPos[1]][1] == 'B':
-            movementsRaycast(1,1)
-            movementsRaycast(-1,1)
-            movementsRaycast(1,-1)
-            movementsRaycast(-1,-1)
-        #===QWEEN===#
+            movementsRaycast(1,1,8)
+            movementsRaycast(-1,1,8)
+            movementsRaycast(1,-1,8)
+            movementsRaycast(-1,-1,8)
+        #===Qween===#
         if board[cursorPos[0]][cursorPos[1]][1] == 'Q':
-            movementsRaycast(1,1)
-            movementsRaycast(-1,1)
-            movementsRaycast(1,-1)
-            movementsRaycast(-1,-1)
-            movementsRaycast(1,0)
-            movementsRaycast(0,1)
-            movementsRaycast(-1,0)
-            movementsRaycast(0,-1)
-        #===PAWN===#
+            movementsRaycast(1,1,8)
+            movementsRaycast(-1,1,8)
+            movementsRaycast(1,-1,8)
+            movementsRaycast(-1,-1,8)
+            movementsRaycast(1,0,8)
+            movementsRaycast(0,1,8)
+            movementsRaycast(-1,0,8)
+            movementsRaycast(0,-1,8)
+        #===kNight===#
+        if board[cursorPos[0]][cursorPos[1]][1] == 'N':
+            movementsRaycast(1,2,2)
+            movementsRaycast(-1,2,2)
+            movementsRaycast(1,-2,2)
+            movementsRaycast(-1,-2,2)
+            movementsRaycast(2,1,2)
+            movementsRaycast(2,-1,2)
+            movementsRaycast(-2,-1,2)
+            movementsRaycast(-2,1,2)
+        #===King===#
+        if board[cursorPos[0]][cursorPos[1]][1] == 'K':
+            movementsRaycast(1,1,2)
+            movementsRaycast(1,-1,2)
+            movementsRaycast(-1,-1,2)
+            movementsRaycast(-1,1,2)
+            movementsRaycast(0,1,2)
+            movementsRaycast(0,-1,2)
+            movementsRaycast(1,0,2)
+            movementsRaycast(-1,0,2)
+        #===Pawn===#
         if board[cursorPos[0]][cursorPos[1]][1] == 'P':
             if turn == 'w': direction = -1
             elif turn == 'b': direction = 1
-            if board[selectedPos[0]+direction][selectedPos[1]-1][0] == opositeTurn(turn): # can eat left
-                positions.append([selectedPos[0]+direction,selectedPos[1]-1])
-            if board[selectedPos[0]+direction][selectedPos[1]+1][0] == opositeTurn(turn): # can eat right
-                positions.append([selectedPos[0]+direction,selectedPos[1]+1])
             if board[selectedPos[0]+direction][selectedPos[1]][0] == '-': # can move one
                 positions.append([selectedPos[0]+direction,selectedPos[1]])
                 if selectedPos[0] == 6 and board[4][selectedPos[1]][0] == '-' and board[cursorPos[0]][cursorPos[1]][0] == 'w': # can move two
                     positions.append([4,selectedPos[1]])
                 if selectedPos[0] == 1 and board[3][selectedPos[1]][0] == '-' and board[cursorPos[0]][cursorPos[1]][0] == 'b': # can move two
                     positions.append([3,selectedPos[1]])
+            if board[selectedPos[0]+direction][selectedPos[1]+1][0] == opositeTurn(turn): # can eat right
+                positions.append([selectedPos[0]+direction,selectedPos[1]+1])
+            if board[selectedPos[0]+direction][selectedPos[1]-1][0] == opositeTurn(turn): # can eat left
+                positions.append([selectedPos[0]+direction,selectedPos[1]-1])
 
     elif selectedPos != [-1,-1]:
         if [cursorPos[0],cursorPos[1]] in positions:
             board[cursorPos[0]][cursorPos[1]] = board[selectedPos[0]][selectedPos[1]]
             board[selectedPos[0]][selectedPos[1]] = '-'
             turn = opositeTurn(turn)
-        selectedPos = [-1,-1]
-
-    print(positions)
+        selectedPos = [-1,-1]        
         
-        
-def color(string, color=''):
+def color(string, color=''): #set colors
         #https://stackabuse.com/how-to-print-colored-text-in-python/
     if color == 'white':
         string = '\u001b[48;5;244m' + string + '\u001b[0m'
@@ -170,55 +188,29 @@ def color(string, color=''):
     return string
 
 def render(board):
+    # clearScreen()
     global selectedPos
     global positions
-    global movablePos
     global LUT
-    # MoveLut = {
-    #     'bP': [[-1,-1],[-1,1],[-1,0],[-2,0]],
-    #     'bN': [[1,-2],[1,2],[2,1],[2,-1],[-1,-2],[-1,2],[-2,1],[-2,-1]],
-    #     'bB': [[1,1],[1,-1],[-1,1],[-1,-1],[2,2],[2,-2],[-2,2],[-2,-2],[3,3],[3,-3],[-3,3],[-3,-3],[4,4],[4,-4],[-4,4],[-4,-4],[5,5],[5,-5],[-5,5],[-5,-5],[6,6],[6,-6],[-6,6],[-6,-6],[7,7],[7,-7],[-7,7],[-7,-7]],
-    #     'bR': [[0,-1],[0,-2],[0,-3],[0,-4],[0,-5],[0,-6],[0,-7],[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[-1,0],[-2,0],[-3,0],[-4,0],[-5,0],[-6,0],[-7,0],[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0]],
-    #     'bQ': [[0,-1],[0,-2],[0,-3],[0,-4],[0,-5],[0,-6],[0,-7],[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[-1,0],[-2,0],[-3,0],[-4,0],[-5,0],[-6,0],[-7,0],[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0],[1,1],[2,2],[3,3],[4,4],[5,5],[6,6],[7,7],[-1,1],[-2,2],[-3,3],[-4,4],[-5,5],[-6,6],[-7,7],[1,-1],[2,-2],[3,-3],[4,-4],[5,-5],[6,-6],[7,-7],[-1,-1],[-2,-2],[-3,-3],[-4,-4],[-5,-5],[-6,-6],[-7,-7]],
-    #     'bK': [[-1,0],[1,0],[0,1],[0,-1],[1,-1],[-1,-1],[-1,1],[1,1]],
-    #     'wP': [[1,-1],[1,1],[1,0],[2,0]],
-    #     'wN': [[1,-2],[1,2],[2,1],[2,-1],[-1,-2],[-1,2],[-2,1],[-2,-1]],
-    #     'wB': [[1,1],[1,-1],[-1,1], [-1,-1],[2,2],[2,-2],[-2,2],[-2,-2], [3,3],[3,-3],[-3,3],[-3,-3],[4,4],[4,-4],[-4,4],[-4,-4],[5,5],[5,-5],[-5,5],[-5,-5],[6,6],[6,-6],[-6,6],[-6,-6],[7,7],[7,-7],[-7,7],[-7,-7]],
-    #     'wR': [[0,-1],[0,-2],[0,-3],[0,-4],[0,-5],[0,-6],[0,-7],[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[-1,0],[-2,0],[-3,0],[-4,0],[-5,0],[-6,0],[-7,0],[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0]],
-    #     'wQ': [[0,-1],[0,-2],[0,-3],[0,-4],[0,-5],[0,-6],[0,-7],[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[-1,0],[-2,0],[-3,0],[-4,0],[-5,0],[-6,0],[-7,0],[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0],[1,1],[2,2],[3,3],[4,4],[5,5],[6,6],[7,7],[-1,1],[-2,2],[-3,3],[-4,4],[-5,5],[-6,6],[-7,7],[1,-1],[2,-2],[3,-3],[4,-4],[5,-5],[6,-6],[7,-7],[-1,-1],[-2,-2],[-3,-3],[-4,-4],[-5,-5],[-6,-6],[-7,-7]],
-    #     'wK': [[-1,0],[1,0],[0,1],[0,-1],[1,-1],[-1,-1],[-1,1],[1,1]],
-    # }
-    #clearScreen()
     for i in range(8):
         for j in range(8):
             item = LUT[board[i][j]]
+            if selectedPos != [-1,-1] and [i,j] in positions:
+                if board[i][j][0] == opositeTurn(turn) or selectedPos == turn:
+                    item = color(item, 'red') #Movable
+                else:
+                    item = color(item, 'lightMove') #Movable
             if i == cursorPos[0] and j == cursorPos[1] and cursorPos == selectedPos:
                 item = color(item, 'yellow') #Selected and cursor in same position
             if i == selectedPos[0] and j == selectedPos[1]:
                 item = color(item, 'green') # selected
             if i == cursorPos[0] and j == cursorPos[1]:
                 item = color(item, 'orange') # Cursor
-            
-            #if selectedPos != [-1,-1] and movablePos in MoveLut[board[selectedPos[0]][selectedPos[1]]]:
-            if selectedPos != [-1,-1] and [i,j] in positions:
-                if board[i][j][0] == opositeTurn(turn):
-                    item = color(item, 'red') #Movable
-                else:
-                    item = color(item, 'lightMove') #Movable
+
             if (i+j) % 2 == 0:
                 item = color(item, 'white')
             print(item,end='')
         print()
-
-# board = [['-',  '-',  '-',  '-',  '-',  '-',  '-',  '-' ],
-#          ['-',  '-',  '-',  '-',  '-',  '-',  '-',  '-' ],
-#          ['-',  '-',  '-',  '-',  '-',  'wR',  '-',  '-' ],
-#          ['-',  '-',   '-',  '-',  '-',  '-',  '-',  '-' ],
-#          ['-',  '-',  'bB',  '-',   '-',  '-',  '-',  '-' ],
-#          ['-',  '-',  '-',  'wB',  '-',  '-',  '-',  '-' ],
-#          ['-',  '-',  '-',  '-',  '-',  '-',  '-',  '-' ],
-#         ['-',  '-',  '-',  '-',  '-',  '-',  '-',  '-' ]]
-
-
+    
 with Listener(on_press=onPress) as l:
     l.join()
